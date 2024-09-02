33b84a692ecd0f3b5f87d307824b409021c7a06f
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-09-02 12:26:11 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    String nome = "Renato";
+    int dia = 17;
+    int mês = 4;
+    int ano = 2009;
+    double pi = 3.14159;
+   
+    System.out.printf("O valor de pi é: %.4f ", pi);
+    
+  }
+
+  
+}
